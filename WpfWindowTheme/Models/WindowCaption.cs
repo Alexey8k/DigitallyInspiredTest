ad14a5d4c7fd77b8ada6243f598c60de3ca21771
Alexey8k <alexey.k8developer@gmail.com>
2019-08-26 17:29:38 +0300

    start of window style creation process

---
+++ WpfWindowTheme/Models/WindowCaption.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace WpfWindowTheme.Models
+{
+    internal class WindowCaption
+    {
+        public double Height { get; set; }
+
+        public Brush Background { get; set; }
+    }
+}
---
+++ WpfWindowTheme/ViewModels/WindowStyleViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Expression.Interactivity.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using WpfWindowTheme.Models;
+
+namespace WpfWindowTheme.ViewModels
+{
+    internal class WindowStyleViewModel
+    {
+        public WindowCaption WindowCaption { get; set; } = new WindowCaption { Height = 32, Background = Brushes.RoyalBlue };
+
+        public ICommand CloseWindowCommand => new ActionCommand(
+            sender => InvokeIfIsWindow(sender, window => SystemCommands.CloseWindow(window)));
+
+        public ICommand MinimizeWindowCommand => new ActionCommand(
+            sender => InvokeIfIsWindow(sender, window => SystemCommands.MinimizeWindow(window)));
+
+        public ICommand MaximizeCommand => new ActionCommand(
+            sender => InvokeIfIsWindow(sender, window => SystemCommands.MaximizeWindow(window)));
+
+        public ICommand RestoreWindowCommand => new ActionCommand(
+            sender => InvokeIfIsWindow(sender, window => SystemCommands.RestoreWindow(window)));
+
+        private void InvokeIfIsWindow(object sender, Action<Window> action)
+        {
+            if (sender is Window window) action(window);
+        }
+    }
+}
