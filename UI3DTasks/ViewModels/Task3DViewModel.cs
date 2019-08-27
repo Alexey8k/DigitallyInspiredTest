f40c62ec334942dccd34ed5fcf276e19fda118f3
Alexey8k <alexey.k8developer@gmail.com>
2019-08-27 23:29:03 +0300

    started the implementation of 3D Task

---
+++ UI3DTasks/ViewModels/Task3DViewModel.cs
@@ -0,0 +1,33 @@
+﻿using HelixToolkit.Wpf;
+using Microsoft.Expression.Interactivity.Core;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media.Media3D;
+
+namespace UI3DTasks.ViewModels
+{
+    internal class Task3DViewModel
+    {
+        public Model3DGroup Model3D { get; private set; }
+
+        public ICommand LoadCommand => new ActionCommand(() =>
+        {
+            var openFileDialog = new OpenFileDialog { DefaultExt = ".obj", Filter = "3D Model|*.obj" };
+            if (openFileDialog.ShowDialog() ?? false)
+            {
+                if (Model3D != null) Model3D = null;
+                Model3D = new ModelImporter().Load(openFileDialog.FileName);
+            }
+        });
+
+        public ICommand ClearCommand => new ActionCommand(() =>
+        {
+            Model3D = null;
+        });
+    }
+}
--- UI3DTasks/Views/Task3DView.xaml.cs
+++ UI3DTasks/Views/Task3DView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace UI3DTasks
+namespace UI3DTasks.Views
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для TaskView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Task3DView : Window
     {
-        public MainWindow()
+        public Task3DView()
         {
             InitializeComponent();
         }
