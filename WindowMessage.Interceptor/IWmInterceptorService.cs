1281b6c98e6521a3c2341e1df5d3f9efcb738feb
Alexey8k <alexey.k8developer@gmail.com>
2019-09-18 05:08:44 +0300

    added files
    added WmInterceptorService. WmHandler, WmMessage, IWmInterceptorService

---
+++ WindowMessage.Interceptor/IWmInterceptorService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowMessage.Interceptor
+{
+    public interface IWmInterceptorService
+    {
+        void AddHandler(WmHandler wmHandler);
+    }
+}
---
+++ WindowMessage.Interceptor/WmHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowMessage.Interceptor
+{
+    public abstract class WmHandler
+    {
+        public IntPtr ReturnValue { get; protected set; } = IntPtr.Zero;
+
+        public abstract int Massage { get; }
+
+        public abstract void Handler(ref WmMessage wmMessage);
+    }
+}
